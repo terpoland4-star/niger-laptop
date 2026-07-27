c604a3cfdb220728e74e373b786a06351f87ebaa
terpoland4-star <terpoland4-star@users.noreply.github.com>
2026-07-27 19:02:21 +0000

    Fix: transmission de la quantité réelle du panier dans les commandes

--- server/routes/orders.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/orderController');
-
-router.post('/', ctrl.createOrder);
-router.get('/my-orders', ctrl.getMyOrders);
-router.get('/:id', ctrl.getOrder);
-
-module.exports = router;

ed1008e3cdc1bbd5d0c6a93276c6c39b2e986d2b
terpoland4-star <terpoland4-star@users.noreply.github.com>
2026-08-26 00:28:03 +0000

    Ajout colonnes motif_transaction, long_transaction, lat_transaction, url_callback à nita_transactions

---
+++ server/db/migrations/0006_messy_stature.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `nita_transactions` ADD `motif_transaction` text;--> statement-breakpoint
+ALTER TABLE `nita_transactions` ADD `long_transaction` text;--> statement-breakpoint
+ALTER TABLE `nita_transactions` ADD `lat_transaction` text;--> statement-breakpoint
+ALTER TABLE `nita_transactions` ADD `url_callback` text;
